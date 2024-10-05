148b9ff4baa4037beba4c2397bc6a25182e1b8a2
DlonRash <coolboisz123@gmail.com>
2024-10-05 12:35:17 -0500

    changed some of the high score code

--- game/UI/ui_root.gd
+++ game/UI/ui_root.gd
@@ -1,40 +1,36 @@
 extends Control
 
-# score and essence meters
+# Score and essence meters
 var score: int = 0
 var essence_meter_1: int = 0
 var essence_meter_2: int = 0
 var essence_meter_3: int = 0
 
-var high_score: int = 0
-const HIGH_SCORE_FILE: String = "user://high_score.txt"
+var high_score: int = 0  # Simple high score variable
 
 const MAX_ESSENCE_METER_1: int = 100
 const MAX_ESSENCE_METER_2: int = 100
 const MAX_ESSENCE_METER_3: int = 100
 
-# Called when the node is added to the scene
 func _ready():
 	update_score_label()
 	update_meters()
-	load_high_score()
 	$FailStateMenu.visible = false  # Hide fail menu initially
-	$UpgradeMenu.visible = false    # Hide upgrade menu initially
+	$UpgradeMenu.visible = false      # Hide upgrade menu initially
 
-
-# Function to add points to the score
 func add_score(points: int):
 	score += points
 	update_score_label()
 	check_high_score()
 
+func check_high_score():
+	if score > high_score:
+		high_score = score  # Update high score if current score is higher
 
-# Update the score label
 func update_score_label():
 	$ScoreLabel.text = "Score: " + str(score)
 	$HighScoreLabel.text = "High Score: " + str(high_score)
 
-# Functions to update essence meters
 func add_to_essence_meter_1(amount: int):
 	essence_meter_1 = min(essence_meter_1 + amount, MAX_ESSENCE_METER_1)
 	update_meters()
@@ -47,19 +43,16 @@ func add_to_essence_meter_3(amount: int):
 	essence_meter_3 = min(essence_meter_3 + amount, MAX_ESSENCE_METER_3)
 	update_meters()
 
-# Update the meters display
 func update_meters():
 	$EssenceMeter_1.value = essence_meter_1
 	$EssenceMeter_2.value = essence_meter_2
 	$EssenceMeter_3.value = essence_meter_3
 
-# Show fail state menu
 func show_fail_menu():
 	$FailStateMenu.visible = true
 	$MainGameUI.visible = false  # Hide the main game UI
-	$UpgradeMenu.visible = false # Hide upgrade menu when failed
+	$UpgradeMenu.visible = false   # Hide upgrade menu when failed
 
-# Restart the game
 func restart_game():
 	score = 0
 	essence_meter_1 = 0
@@ -70,37 +63,24 @@ func restart_game():
 	$FailStateMenu.visible = false
 	$MainGameUI.visible = true
 
-# Quit the game
 func quit_game():
 	get_tree().quit()
 
-# Start the game from the main menu
 func start_game():
 	$MainMenu.visible = false
 	$MainGameUI.visible = true
 
-# Open the upgrade menu
 func open_upgrade_menu():
 	$UpgradeMenu.visible = true
 	$MainGameUI.visible = false
 
-# Close the upgrade menu and go back to the game
 func close_upgrade_menu():
 	$UpgradeMenu.visible = false
 	$MainGameUI.visible = true
 
-# Check fail state condition (example function to monitor fail condition)
 func check_fail_condition():
 	if essence_meter_1 <= 0 and essence_meter_2 <= 0 and essence_meter_3 <= 0:
 		show_fail_menu()
-
-#save hight score to a file
-func save_high_score():
-	var file = File.new()
-	file.store_var(high_score)
-	file.close()
-
-
 
 func _on_quit_button_pressed() -> void:
 	quit_game()
@@ -108,8 +88,8 @@ func _on_quit_button_pressed() -> void:
 func _on_retry_button_pressed() -> void:
 	restart_game()
 
-func _on_UpgradeButton_pressed():
+func _on_upgrade_button_pressed():
 	open_upgrade_menu()
 
-func _on_CloseUpgradeMenu_pressed():
+func _on_close_upgrade_menu_pressed():
 	close_upgrade_menu()
