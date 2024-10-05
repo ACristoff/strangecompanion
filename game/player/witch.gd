6c65bb4966a24b64d5378c64588dba6400c138b3
Alex Cristoffanini <acristoffanini@gmail.com>
2024-10-04 19:22:11 -0700

    witch code init

--- game/player/witch.gd
+++ game/player/witch.gd
@@ -1,6 +1,20 @@
 extends CharacterBody2D
 
 @onready var companion = $Companion
+@onready var sprite = $Sprite2D
+
+@export var SPEED = 300
+@export var health = 100
+
+enum SHOOT_MODE {
+	BIG, 
+	SPREAD, 
+	HOMING
+}
+
+var current_mode = SHOOT_MODE.HOMING
+
+var face_right = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,6 +23,19 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var horizontalDirection = Input.get_axis("move_left", "move_right")
+	var verticalDirection = Input.get_axis("move_up", "move_down")
+	
+	if horizontalDirection:
+		sprite.flip_h = (horizontalDirection == -1)
+		face_right = (horizontalDirection == 1)
+		pass
+	
+	velocity.x = horizontalDirection * SPEED
+	velocity.y = verticalDirection * SPEED
+	
+	velocity = velocity.normalized()
+	move_and_slide()
 	pass
 
 #Shooting from companion node
