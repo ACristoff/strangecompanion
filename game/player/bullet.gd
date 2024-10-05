c5929d1c429b53c0db379b9afb810cc409df255e
kinteyoi <calebazlin5@gmail.com>
2024-10-05 02:24:22 -0500

    crystal bullet

--- game/player/bullet.gd
+++ game/player/bullet.gd
@@ -6,7 +6,7 @@ extends Node2D
 #var rotation_vector = Vector2.UP.rotated(rotation)
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$AnimationPlayer.play("crystal")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
