cc48a3a00d6301c468329952222397c464c65606
kinteyoi <calebazlin5@gmail.com>
2024-10-05 15:28:56 -0500

    bars improvement

--- game/JuicyDetails.gd
+++ game/JuicyDetails.gd
@@ -1,5 +1,13 @@
 extends Node
 
+var bar_1 = 0
+var bar_2 = 0
+var bar_3 = 0
+var outfit = 0
+var score = 0
+
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
