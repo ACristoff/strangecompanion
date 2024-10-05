09e47f1f434eaef4c14e9b8bb35f2ad7b56c4be1
ChaneyWilliams <chaneyw2004@gmail.com>
2024-10-05 13:33:05 -0500

    Spawns are more evenly distributed

--- game/enemies/GlobalPos.gd
+++ game/enemies/GlobalPos.gd
@@ -4,6 +4,13 @@ var player = null
 var enemy1 = preload("res://game/enemies/base_enemy.tscn")
 var enemy2 = preload("res://game/enemies/enemy_2.tscn")
 
+# farthest it can spawn 
+var minPosX = -270
+var maxPosX = 1350
+
+var minPosY = -150
+var maxPosY = 1250
+
 # returns the position of node so the enemy can chase it
 func instance_node(node, location, parent):
 	var node_instance = node.instantiate()
@@ -15,10 +22,10 @@ func instance_node(node, location, parent):
 #spawn enemies randomly
 func _on_enemy_spawn_timer_timeout() -> void:
 	# i have no idea what the ranges should be
-	var enemy_pos = Vector2(randi_range(-150, 1250), randi_range(-150, 800))
+	var enemy_pos = Vector2(randi_range(minPosX, maxPosX), randi_range(minPosY, maxPosY))
 	
-	while enemy_pos.x < 1200 and enemy_pos.x > -75 or enemy_pos.y > -75 and enemy_pos.y < 750:
-		enemy_pos = Vector2(randi_range(-150, 1250), randi_range(-150, 800))
+	while enemy_pos.x > 0 and enemy_pos.x < 1150:
+		enemy_pos = Vector2(randi_range(minPosX, maxPosX), enemy_pos.y)
 	
 	var choice = randi_range(1,10)
 	if choice <= 5:
