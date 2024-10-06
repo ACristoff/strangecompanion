c6b7eadc807bc0a252d504a21fa51719dbc76448
kinteyoi <calebazlin5@gmail.com>
2024-10-05 19:25:12 -0500

    shop

--- game/UI/upgradesmovement.gd
+++ game/UI/upgradesmovement.gd
@@ -9,3 +9,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _shopbegin():
+	var tween = create_tween()
+	tween.tween_property($CanvasLayer/Node2D, "global_position", $lodestart.global_position, 0)
+	pass
