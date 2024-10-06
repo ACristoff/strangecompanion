3d00622deeeb73f09667431325116e3b512e4a8c
kinteyoi <calebazlin5@gmail.com>
2024-10-06 01:57:00 -0500

    bars

--- game/enemies/base_enemy.gd
+++ game/enemies/base_enemy.gd
@@ -6,27 +6,31 @@ var biggie = preload("res://Assets/Enemy/Small_Pumpkin_SpriteSheet.PNG")
 var speed = 100
 var velocity = Vector2()
 var health = 10
+var type = null
 
 var enemy_types =  {
 	"biggie": {
+		"energy":10,
 		"speed": 100,
 		"health": 20,
 		"scale": Vector2(1,1),
 		"sprite": null
 	},
 	"smalls": {
+		"energy":5,
 		"speed": 200,
 		"health": 6,
 		"scale": Vector2(0.6,0.6)
 	}
 }
 
-func construct_enemy(type):
+func construct_enemy(new_type):
 	#print(type)
-	health = enemy_types[type].health
-	speed = enemy_types[type].speed
-	$Sprite2D.scale = $Sprite2D.scale * enemy_types[type].scale
-	$CollisionShape2D.scale = $CollisionShape2D.scale * enemy_types[type].scale
+	type = new_type
+	health = enemy_types[new_type].health
+	speed = enemy_types[new_type].speed
+	$Sprite2D.scale = $Sprite2D.scale * enemy_types[new_type].scale
+	$CollisionShape2D.scale = $CollisionShape2D.scale * enemy_types[new_type].scale
 
 func _process(delta):
 	if GlobalPos.player != null:
@@ -35,6 +39,6 @@ func _process(delta):
 
 func take_damage(damage):
 	health -= damage
-	if health <= 0:
-		
+	if health <= 0 && type != null:
+		JuicyDetails._addenergy(enemy_types[type].energy)
 		queue_free()
