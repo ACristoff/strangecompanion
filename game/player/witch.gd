2e7f4523802a01604246aece5c0444a4f207228b
kinteyoi <calebazlin5@gmail.com>
2024-10-05 21:45:39 -0500

    redwitchwhitewth

--- game/player/witch.gd
+++ game/player/witch.gd
@@ -86,9 +86,6 @@ func _process(delta):
 	var verticalDirection = Input.get_axis("ui_up", "ui_down")
 	if horizontalDirection:
 		sprite.flip_h = (horizontalDirection == -1)
-		$outfitcrystal/BlueOutfitWitch.flip_h = (horizontalDirection == -1)
-		$outfitcrystal/BlueScarfWitch.flip_h = (horizontalDirection == -1)
-		$outfitcrystal/BlueHatWitch.flip_h = (horizontalDirection == -1)
 		face_right = (horizontalDirection == 1)
 		pass
 	velocity.x = horizontalDirection * SPEED
