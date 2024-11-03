cddf3e1a7c7a1196d5c11d30995fb3ac2213b83d
Alex Cristoffanini <acristoffanini@gmail.com>
2024-11-03 14:39:34 -0800

    enemy planning

--- game/enemies/base_enemy_3d.gd
+++ game/enemies/base_enemy_3d.gd
@@ -1,1 +1,15 @@
+class_name BaseEnemy
 extends RigidBody3D
+
+var health = 100
+var attack = 10
+var speed = 5
+var target = GameManager.player
+
+
+#Take Damage
+	#Update HealthBar
+
+#Do Damage
+
+#Die
--- game/world/enemy_manager.gd
+++ game/world/enemy_manager.gd
@@ -6,18 +6,13 @@ extends Node3D
 @onready var mob_spawn_box = $MobSpawnBox
 var basic_enemy = preload("res://game/enemies/base_enemy_3d.tscn")
 
+var current_wave = 0
+
 
 func _process(_delta):
 	if GameManager.player != null:
-		#print()
 		mob_spawn_box.global_position = GameManager.player.global_position + Vector3(-10, 0, -10)
 		pass
-	
-	#mob_spawn_box.global_positon = player.global_position
-	#if player != null:
-		#prints(player.global_position)
-		#prints(self.global_position)
-		#pass
 	pass
 
 func _on_mob_timer_timeout():
@@ -25,5 +20,4 @@ func _on_mob_timer_timeout():
 	mob_spawn_location.progress_ratio = randf()
 	mob.global_position = mob_spawn_location.global_position
 	add_child(mob)
-	prints("Mob spawn", mob_spawn_location.global_position)
-	pass # Replace with function body.
+	#prints("Mob spawn", mob_spawn_location.global_position)
