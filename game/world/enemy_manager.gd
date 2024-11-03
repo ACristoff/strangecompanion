305d7e4e68d1981e9c91f8e632894847fe35d1e8
Alex Cristoffanini <acristoffanini@gmail.com>
2024-11-03 13:32:06 -0800

    base enemy spawning

--- game/world/enemy_manager.gd
+++ game/world/enemy_manager.gd
@@ -1,22 +1,29 @@
 
 extends Node3D
 
+#@export var player = Node3D
 @onready var mob_spawn_location = $MobSpawnBox/MobSpawnLocation
+@onready var mob_spawn_box = $MobSpawnBox
+var basic_enemy = preload("res://game/enemies/base_enemy_3d.tscn")
 
-var basic_enemy = null
 
-
+func _process(_delta):
+	if GameManager.player != null:
+		#print()
+		mob_spawn_box.global_position = GameManager.player.global_position + Vector3(-10, 0, -10)
+		pass
+	
+	#mob_spawn_box.global_positon = player.global_position
+	#if player != null:
+		#prints(player.global_position)
+		#prints(self.global_position)
+		#pass
+	pass
 
 func _on_mob_timer_timeout():
-	
-	var mob = null
-	
-	#spawn a guy
+	var mob = basic_enemy.instantiate()
 	mob_spawn_location.progress_ratio = randf()
-	
-	#mob.position = mob_spawn_location
-	
-	#add_child(mob)
-	
-	prints("Mob spawn", mob_spawn_location.position)
+	mob.global_position = mob_spawn_location.global_position
+	add_child(mob)
+	prints("Mob spawn", mob_spawn_location.global_position)
 	pass # Replace with function body.
